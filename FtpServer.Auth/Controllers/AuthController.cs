72214254f18b96de7b045803c82c08717e379b37
dimak <dimka2010@gmail.com>
2025-07-19 14:14:29 +0300

    feat: Added basic AuthController with info and authenticate endpoints

---
+++ FtpServer.Auth/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using FtpServer.Auth.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FtpServer.Auth;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController: ControllerBase
+{
+    [HttpGet("info")]
+    public IActionResult GetInfo()
+    {
+        return Ok(new
+        {
+            Service = "FTP Authentication Service",
+            Version = "1.0.0",
+            Status = "Running",
+            Time = DateTime.UtcNow
+        });
+    }
+
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody] AuthRequest authRequest)
+    {
+        // While simple verify
+        if (authRequest.Username == "demo" || authRequest.Username == "test")
+        {
+            var user = new User
+            {
+                Id = 1,
+                Username = "demo",
+                Email = "demo@example.com",
+                IsActive = true
+            };
+
+            return Ok(new AuthResponse
+            {
+                IsSuccess = true,
+                User = user,
+                Message = "Authentication successful"
+            });
+        }
+
+        return BadRequest(new AuthResponse
+       {
+            IsSuccess=false,
+            Message = "Invalid credentials"
+        });
+    }
+}
--- FtpServer.Auth/Program.cs
+++ FtpServer.Auth/Program.cs
@@ -4,14 +4,40 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer(); // Essential for Swagger to discover endpoints
 
+// Configure Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "V1",
+        Title = "FtpServer Auth API",
+        Description = "Auth service for Ftp Server",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Dima Karpenko, dimak",
+            Email = "dimka2010@gmail.com",
+            Url = new Uri("https://github.com/karpenkodima")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-   
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Auth API V1");
+
+        /*
+         *  http://localhost:5000/ to serve Swagger UI at the application's root 
+         */
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
